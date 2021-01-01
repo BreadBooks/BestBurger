453f69a628a4b739228996c39a7faada3c2a73fb
Brett B <65505937+BreadBooks@users.noreply.github.com>
2020-12-31 20:22:45 -0600

    Bug Fix & Small Improvements

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -3,14 +3,16 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
-    res.redirect("/burgers");
+burger.all(function (burgerData) {
+    res.render("index", { burger_data: burgerData });
+});
 });
 
-router.get("/burgers", function (req, res) {
-    burger.all(function (burgerData) {
-        res.render("index", { burger_data: burgerData });
-    });
-});
+// router.get("/burgers", function (req, res) {
+//     burger.all(function (burgerData) {
+//         res.render("index", { burger_data: burgerData });
+//     });
+// });
 
 router.post("/burgers/create", function (req, res) {
     if (!req.body.burger_name) {
