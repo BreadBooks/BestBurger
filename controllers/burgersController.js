8b1a778747e70543fab721d60a876bcf53920a39
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-01-02 13:22:47 -0600

    maybe fix

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,7 +5,7 @@ var burger = require("../models/burger.js");
 router.get("/", function (req, res) {
 burger.all(function (burgerData) {
     res.render("index", { burger_data: burgerData });
-});
+    });
 });
 
 router.post("/burgers/create", function (req, res) {
